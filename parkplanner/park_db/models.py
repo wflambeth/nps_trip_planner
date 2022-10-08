41fae1f05669ba5404aa588fc3ad7669b5c0292d
will_lambeth <wflambeth@gmail.com>
2022-10-07 17:08:18 -0700

    Build db migrations, create db admin credentials

--- parkplanner/park_db/models.py
+++ parkplanner/park_db/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Park (models.Model):
+    def __str__(self):
+        return self.park_code
+    
     park_code = models.CharField(max_length=10, unique=True)
     park_fullname = models.CharField(max_length=512, default="")
 
--- parkplanner/parkplanner/settings.py
+++ parkplanner/parkplanner/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "park_db.apps.ParkDbConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
