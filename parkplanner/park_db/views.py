af8c82af6e87a99ab5faf80a80d108e0046db87a
will_lambeth <wflambeth@gmail.com>
2022-10-07 18:19:24 -0700

    Include API key secret

--- parkplanner/park_db/views.py
+++ parkplanner/park_db/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import Credentials
+
+API_KEY_NPS = Credentials.API_KEY_NPS
 
 def index(request):
     return HttpResponse("Closed due to wildfire smoke. Check back soon!")
