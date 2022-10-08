5eec5c7846db9e7b714ebdc03128ffb08ffcbf3c
will_lambeth <wflambeth@gmail.com>
2022-10-07 18:46:50 -0700

    Build view for returning park fullname

--- parkplanner/park_db/urls.py
+++ parkplanner/park_db/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('<str:park_code>/fullname', views.fullname, name="fullname")
 ]
--- parkplanner/park_db/views.py
+++ parkplanner/park_db/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django import http
+import json
 import Credentials
+from park_db.models import Park
 
+# for future dynamic update calls 
+# (or we can just implement this as middleware)
 API_KEY_NPS = Credentials.API_KEY_NPS
+PARKS_ENDPOINT = "https://developer.nps.gov/api/v1/parks?"
 
-def index(request):
-    return HttpResponse("Closed due to wildfire smoke. Check back soon!")
+# request must be formatted: /park_db/(park_code)/fullname
+
+def fullname(request, park_code):
+    if request.method != 'GET':
+        return http.HttpResponseNotAllowed(['GET'])
+
+    try:
+        park = Park.objects.filter(park_code=park_code)
+        if not park:
+            return http.HttpResponseNotFound("Park " + park_code + " not found.")
+
+        return http.JsonResponse({
+            "park_code": park.park_code,
+            "park_fullname": park.park_fullname
+        })        
+    
+    except:
+        e = sys.exc_info()[0]
+        # This is a bad idea and only in here to DEBUG
+        http.HttpResponseServerError(e)
+
