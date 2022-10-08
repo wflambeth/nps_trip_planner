2bf4d2e2c2e8dec34b4b9f79ec6e9f0a093e4d87
will_lambeth <wflambeth@gmail.com>
2022-10-08 11:08:07 -0700

    Implement event search endpoint

--- parkplanner/park_db/urls.py
+++ parkplanner/park_db/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:park_code>/fullname', views.fullname, name="fullname")
+    path('<str:park_code>/fullname', views.fullname, name="fullname"),
+    path('<str:park_code>/events/<str:date>', views.events, name="events")
 ]
