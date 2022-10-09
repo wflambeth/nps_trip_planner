4dcd92d80061f66e853d363e569169cec46e590e
Brittaney Nico Davis <nicomoving@gmail.com>
2022-10-09 09:55:35 -0700

    fixed header and footer images

--- UI/parkplanner/src/components/Header.js
+++ UI/parkplanner/src/components/Header.js
@@ -3,12 +3,7 @@ import "../styles/header-styles.css";
 function Header() {
   return (
     <div id="head">
-      <header>
-        <img
-          src="UI/parkplanner/src/styles/images/header-image.png"
-          alt="Header of bears with title National Parks Trip"
-        ></img>
-      </header>
+      <header></header>
     </div>
   );
 }
