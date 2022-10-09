42189cd38b531ae46b74671e7997e1039ccb2b77
Brittaney Nico Davis <nicomoving@gmail.com>
2022-10-09 09:56:44 -0700

    fixed header and footer
    images are fixed

--- UI/parkplanner/src/components/Footer.js
+++ UI/parkplanner/src/components/Footer.js
@@ -3,12 +3,7 @@ import "../styles/footer-styles.css";
 export default function Footer() {
   return (
     <div id="footer">
-      <footer>
-        <img
-          src="footer.png"
-          alt="Footer banner with names of project collaborators, Dan, Will, Monika and Nico"
-        ></img>
-      </footer>
+      <footer></footer>
     </div>
   );
 }
