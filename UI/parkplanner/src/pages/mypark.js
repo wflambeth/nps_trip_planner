9da46e97d65a5d05ad67e95d334dd1c9b8909737
will_lambeth <wflambeth@gmail.com>
2022-10-09 08:13:11 -0700

    Load alert data into mypark page

--- UI/parkplanner/src/pages/mypark.js
+++ UI/parkplanner/src/pages/mypark.js
@@ -11,11 +11,12 @@ export default function Mypark() {
 
   // get park data from API
   const [parkData, setParkData] = useState([]);
+  const [alertData, setAlertData] = useState([]);
 
   useEffect(() => {
     const getParkData = async () => {
       const response = await fetch(
-        `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=y02YQZIE073ut1YQNZMW5vYHnHA4oxLRoG99EIV9` // replace with Django API
+        `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=y02YQZIE073ut1YQNZMW5vYHnHA4oxLRoG99EIV9`
       );
       const data = await response.json();
       setParkData(data.data[0]);
@@ -23,29 +24,38 @@ export default function Mypark() {
     getParkData();
   }, [parkCode]);
 
-  console.log(parkData);
+  useEffect(() => {
+    const getAlertData = async () => {
+      const response = await fetch(
+        `https://developer.nps.gov/api/v1/alerts?parkCode=${parkCode}&api_key=y02YQZIE073ut1YQNZMW5vYHnHA4oxLRoG99EIV9` 
+      );
+      const data = await response.json();
+      setAlertData(data.data[0]);
+    };
+    getAlertData();
+  }, [parkCode]);
+
+  //console.log(parkData);
 
   return (
     <div>
+    <div>
       <h1>{parkData.fullName}</h1>
-      <p className="description">{parkData.description}</p>
-      <p class="directions">{parkData.directionsInfo}</p>
-      <p class="directions-url">
-        <strong>View directions here: </strong> {parkData.directionsUrl}
-      </p>
-      <p class="coordinates-lat">
-        <strong>Latitude: </strong>
-        {parkData.latitude}
-      </p>
-      <p class="coordinates-lon">
-        <strong>Longitude: </strong> {parkData.longitude}
-      </p>
-      <p class="data-url">
-        <strong>Visit this URL for data: </strong> {parkData.url}
-      </p>
-      <p class="weather-info">
-        <strong>Weather information: </strong> {parkData.weatherInfo}
-      </p>
+      <p>{parkData.description}</p>
+      <p>{parkData.directionsInfo}</p>
+      <p>{parkData.directionsUrl}</p>
+      <p>{parkData.latitude}</p>
+      <p>{parkData.longitude}</p>
+      <p>{parkData.url}</p>
+      <p>{parkData.weatherInfo}</p>
+    </div>
+    <div>
+      <h2>Alerts:</h2>
+      <ul>
+        <h3>{alertData.title}</h3>
+        <li>{alertData.description}</li>
+      </ul>
+    </div>
     </div>
   );
 }
